08baaf934fceddfbf2f14b408b0f9eb2df8a875e
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-18 11:20:57 +0000

    [automatic commit] 2025-06-18 11:20:57 UTC 120.20.126.104

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -6,5 +6,6 @@ var CURRENT_BUYER_ID = 11;
 var CURRENT_SELLER_ID = 12;
 
 router.get('/items', async function(req, res, next){
-    const [rows] = await db.query(``)
+    const [rows] = await db.query(`
+        `)
 })
