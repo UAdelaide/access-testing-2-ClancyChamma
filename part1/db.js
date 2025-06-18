2e26b762d509063bd0b6d07b1a6107d3483d1a7c
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-18 11:42:03 +0000

    [automatic commit] 2025-06-18 11:42:03 UTC 120.20.126.104

--- part1/db.js
+++ part1/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
+    socetPath: '/var/run/mysqld/mysqld.sock',
     host: '127.0.0.1',
     user: 'root',
-    password: 'mypassword',
+    password: 'newpassword',
     database: 'textbook_marketplace',
-
 })
 
 module.exports = db;
