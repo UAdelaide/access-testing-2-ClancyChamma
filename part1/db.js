4116b4840e5feea1d1e4076be4863c605acc3dc6
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-18 11:42:50 +0000

    [automatic commit] 2025-06-18 11:42:50 UTC ;; communications error to 208.67.222.222#53: timed out 120.20.126.104

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    socetPath: '/var/run/mysqld/mysqld.sock',
+    // socetPath: '/var/run/mysqld/mysqld.sock',
     host: '127.0.0.1',
     user: 'root',
     password: 'newpassword',
