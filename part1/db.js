e0e8e51c8965601c78efcde81bb71c7d9cbd3d95
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-18 11:41:42 +0000

    [automatic commit] 2025-06-18 11:41:42 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/db.js
+++ part1/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'mypassword',
     database: 'textbook_marketplace',
-    
+
 })
 
 module.exports = db;
