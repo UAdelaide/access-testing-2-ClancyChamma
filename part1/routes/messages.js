9c6c035396c4c015c7e0ec85ab7fb0c0ed79705a
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-19 05:42:25 +0000

    [automatic commit] 2025-06-19 05:42:25 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 115.166.26.52

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -15,6 +15,7 @@ router.get('/items', async function(req, res, next){
     res.json(rows);
 })
 
+router.get('/messages', async function(req, res, next))
 
 router.post('/messages', async function(req, res, next){
     const { bookID, sellerID, message } = req.body;
