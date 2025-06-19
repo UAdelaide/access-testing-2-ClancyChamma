ead11b45c4dd5d6d0abe1bad62c1ba64f50257ae
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-19 06:02:10 +0000

    [automatic commit] 2025-06-19 06:02:09 UTC 115.166.26.52

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db');
 
 var CURRENT_BUYER_ID = 11;
-#
+// var CURRENT_SELLER_ID = 12;
 
 router.get('/items', async function(req, res, next){
     const [rows] = await db.query(`
