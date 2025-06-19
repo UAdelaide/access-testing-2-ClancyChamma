ae730d8dd174dc88cc6098e0f4c75b3ebca6db29
CLancyChamma <a1878312@adelaide.edu.au>
2025-06-19 01:32:22 +0000

    [automatic commit] 2025-06-19 01:32:22 UTC 115.166.26.52

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -20,7 +20,7 @@ router.post('/messages', async function(req, res, next){
     await db.query(`
         INSERT INTO Messages (BuyerID, SellerID, BookID, TextMessage, SentAt)
         VALUES (?, ?, ?, ?, NOW())
-    `, [CURRENT_BUYER_ID, CURRENT_SELLER_ID, bookID, message]);
+    `, [CURRENT_BUYER_ID, sellerID, bookID, message]);
     res.status(201).json({ message: 'Message sent successfully!' });
 });
 
